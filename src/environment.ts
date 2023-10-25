07f7da1ac54ac5ca2b9ff8b21ad9e943d4ba599a
cravelo <cr.cravelo@gmail.com>
2023-10-25 20:44:56 +0200

    addig again environment.ts

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "----",
+    authDomain: "---",
+    databaseURL: "https://paymelater-89bde.firebaseio.com",
+    projectId: "paymelater-89bde",
+    storageBucket: "---",
+    messagingSenderId: "---"
+};
